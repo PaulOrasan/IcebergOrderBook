159ab4e3a4f4420e15acf69d73fdd1c56d3e9f87
Paul Orasan <paul.orasan@tora.com>
2024-03-12 18:18:09 +0200

    Add failing unit tests for trade prediction

--- src/TradePrediction.java
+++ src/TradePrediction.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class TradePrediction {
 
     private final int predictedPrice;
@@ -24,5 +26,29 @@ public class TradePrediction {
 
     public boolean isTradeMatch() {
         return predictedQuantity > 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        TradePrediction that = (TradePrediction) o;
+        return getPredictedPrice() == that.getPredictedPrice() && getPredictedQuantity() == that.getPredictedQuantity()
+                && getTimestamp() == that.getTimestamp();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getPredictedPrice(), getPredictedQuantity(), getTimestamp());
+    }
+
+    @Override
+    public String toString() {
+        return "TradePrediction{" + "predictedPrice=" + predictedPrice + ", predictedQuantity=" + predictedQuantity + ", timestamp=" + timestamp
+                + '}';
     }
 }
---
+++ tests/TradeGeneratorTest.java
@@ -0,0 +1,65 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class TradeGeneratorTest {
+
+    private static final int PRICE = 100;
+    private static final int QUANTITY = 100;
+    private static final int TIMESTAMP = 10;
+
+    @ParameterizedTest
+    @MethodSource("dataProvider")
+    void generatePrediction(final Order aggressiveOrder, final Order passiveOrder, final TradePrediction expectedTrade) {
+        assertEquals(expectedTrade, TradeGenerator.generatePrediction(aggressiveOrder, passiveOrder));
+    }
+
+    private static Stream<Arguments> dataProvider() {
+        Order aggressiveBuyOrder = buildBuyOrder(PRICE, QUANTITY, TIMESTAMP);
+        Order aggressiveSellOrder = buildSellOrder(PRICE, QUANTITY, TIMESTAMP);
+        return Stream.of(
+                scenarioNoTrade(aggressiveBuyOrder, buildSellOrder(PRICE + 10, QUANTITY, TIMESTAMP)),
+                scenario(aggressiveBuyOrder, buildSellOrder(PRICE - 10, QUANTITY, TIMESTAMP / 2), QUANTITY),
+                scenario(aggressiveBuyOrder, buildSellOrder(PRICE - 10, QUANTITY / 2, TIMESTAMP / 2), QUANTITY / 2),
+                scenario(aggressiveBuyOrder, buildSellOrder(PRICE - 10, QUANTITY * 2, TIMESTAMP / 2), QUANTITY),
+                scenarioNoTrade(aggressiveSellOrder, buildBuyOrder(PRICE - 10, QUANTITY, TIMESTAMP)),
+                scenario(aggressiveSellOrder, buildBuyOrder(PRICE + 10, QUANTITY, TIMESTAMP / 2), QUANTITY),
+                scenario(aggressiveSellOrder, buildBuyOrder(PRICE + 10, QUANTITY / 2, TIMESTAMP / 2), QUANTITY / 2),
+                scenario(aggressiveSellOrder, buildBuyOrder(PRICE + 10, QUANTITY * 2, TIMESTAMP / 2), QUANTITY)
+        );
+    }
+
+    private static Arguments scenarioNoTrade(final Order aggressiveOrder, Order passiveOrder) {
+        return scenario(aggressiveOrder, passiveOrder, new TradePrediction(0, 0, 0));
+    }
+
+    private static Arguments scenario(Order aggressiveOrder, Order passiveOrder, int tradeQuantity) {
+        return scenario(aggressiveOrder, passiveOrder, new TradePrediction(passiveOrder.getPrice(), tradeQuantity, aggressiveOrder.getTimestamp()));
+    }
+    private static Arguments scenario(final Order aggressiveOrder, Order passiveOrder, TradePrediction tradePrediction) {
+        return Arguments.of(aggressiveOrder, passiveOrder, tradePrediction);
+    }
+
+    private static Order buildBuyOrder(int price, int quantity, int timestamp) {
+        return buildOrder(Side.BUY, price, quantity, timestamp);
+    }
+
+    private static Order buildSellOrder(int price, int quantity, int timestamp) {
+        return buildOrder(Side.SELL, price, quantity, timestamp);
+    }
+
+    private static Order buildOrder(Side side, int price, int quantity, int timestamp) {
+        final Order order = mock(Order.class);
+        doReturn(Side.BUY.equals(side)).when(order).isBuyOrder();
+        doReturn(price).when(order).getPrice();
+        doReturn(quantity).when(order).getAvailableQuantity();
+        doReturn(timestamp).when(order).getTimestamp();
+        return order;
+    }
+}
